a25b10bf15275c4c692febc8eb46ad0a7b0010e1
Aaron Hill <68696c6c@gmail.com>
2020-03-01 11:30:07 -0700

    Added a getSidesPX method to the SizingConfig.
    Added support for specifying individual side margin to the margined trait.

--- src/traits/margined/index.js
+++ src/traits/margined/index.js
@@ -1,23 +1,54 @@
 import PropTypes from 'prop-types'
 import { css } from '@emotion/core'
 
-import { sizes } from '../../config/sizing'
+import { sizes } from '../../config'
 
 // eslint-disable-next-line import/prefer-default-export
 export const margined = {
   styles: [
-    ({ theme, margin }) => {
-      const spacing = theme.sizing.getPX(margin)
+    ({
+      theme,
+      margin,
+      marginX,
+      marginY,
+      marginTop,
+      marginBottom,
+      marginLeft,
+      marginRight,
+    }) => {
+      const { top, bottom, left, right } = theme.sizing.getSidesPX({
+        all: margin,
+        x: marginX,
+        y: marginY,
+        top: marginTop,
+        bottom: marginBottom,
+        left: marginLeft,
+        right: marginRight,
+      })
       return css`
-        margin-top: ${spacing};
-        margin-right: ${spacing};
-        margin-bottom: ${spacing};
-        margin-left: ${spacing};
+        margin-top: ${top};
+        margin-right: ${bottom};
+        margin-bottom: ${left};
+        margin-left: ${right};
       `
     },
   ],
   propTypes: () => ({
-    margin: PropTypes.oneOf(sizes),
+    margin: PropTypes.oneOf([null, ...sizes]),
+    marginX: PropTypes.oneOf([null, ...sizes]),
+    marginY: PropTypes.oneOf([null, ...sizes]),
+    marginTop: PropTypes.oneOf([null, ...sizes]),
+    marginBottom: PropTypes.oneOf([null, ...sizes]),
+    marginLeft: PropTypes.oneOf([null, ...sizes]),
+    marginRight: PropTypes.oneOf([null, ...sizes]),
   }),
-  defaultProps: margin => ({ margin }),
+  defaultProps: (margin = null, marginX = null, marginY = null) => ({
+    margin,
+    marginX,
+    marginY,
+    marginTop: null,
+    marginBottom: null,
+    marginLeft: null,
+    marginRight: null,
+  }),
 }
