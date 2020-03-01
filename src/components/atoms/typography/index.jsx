04d9578bba12595abae749b4467769a4d1b63712
Aaron Hill <68696c6c@gmail.com>
2020-03-01 14:37:42 -0700

    Added Typography atoms.

---
+++ src/components/atoms/typography/index.jsx
@@ -0,0 +1,147 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { fontStyles } from '../../../config'
+import { chromatic, interactive, typographic } from '../../../traits'
+import { handleProps } from '../../utils'
+
+import * as Styled from './styles'
+
+export const typographicFonts = [
+  ...fontStyles,
+  'blockquote',
+  'samp',
+  'kbd',
+  'pre',
+  'sub',
+  'sup',
+  'del',
+  's',
+]
+
+const Typography = ({ children, font: passedFont, ...others }) => {
+  const props = { ...others }
+  let className = passedFont
+  let Tag
+  let font = passedFont
+  switch (passedFont) {
+    case 'h1':
+      Tag = Styled.H1
+      break
+    case 'h2':
+      Tag = Styled.H2
+      break
+    case 'h3':
+      Tag = Styled.H3
+      break
+    case 'h4':
+      Tag = Styled.H4
+      break
+    case 'h5':
+      Tag = Styled.H5
+      break
+    case 'h6':
+      Tag = Styled.H6
+      break
+    case 'paragraph':
+      Tag = Styled.Paragraph
+      break
+    case 'quote':
+      Tag = Styled.Quote
+      break
+    case 'label':
+      Tag = Styled.Label
+      break
+    case 'link':
+      Tag = Styled.Anchor
+      props.href = props.href ? props.href : '#'
+      props.inactive = false
+      break
+    case 'message':
+      Tag = Styled.Span
+      break
+    case 'caption':
+      Tag = Styled.Span
+      break
+    case 'legal':
+      Tag = Styled.Small
+      break
+    case 'code':
+      Tag = Styled.Code
+      break
+    case 'emphasized':
+      Tag = Styled.EM
+      break
+    case 'strong':
+      Tag = Styled.Strong
+      break
+    case 'variable':
+      Tag = Styled.Var
+      break
+    case 'blockquote':
+      Tag = Styled.BlockQuote
+      font = 'quote'
+      break
+    case 'samp':
+      Tag = Styled.Samp
+      font = 'text'
+      break
+    case 'kbd':
+      Tag = Styled.KBD
+      font = 'code'
+      break
+    case 'pre':
+      Tag = Styled.Pre
+      font = 'code'
+      break
+    case 'sub':
+      Tag = Styled.Sub
+      font = 'text'
+      break
+    case 'sup':
+      Tag = Styled.Sup
+      font = 'text'
+      break
+    case 'del':
+      Tag = Styled.Del
+      font = 'text'
+      break
+    case 's':
+      Tag = Styled.Strikethrough
+      font = 'text'
+      break
+    default:
+      Tag = Styled.Span
+      font = 'text'
+      className = 'text'
+  }
+  return (
+    <Tag {...handleProps(props, className)} font={font}>
+      {children}
+    </Tag>
+  )
+}
+
+Typography.propTypes = {
+  ...chromatic.propTypes(),
+  ...interactive.propTypes(),
+  ...typographic.propTypes(),
+  font: PropTypes.oneOf(typographicFonts),
+}
+
+Typography.defaultProps = {
+  ...chromatic.defaultProps('background'),
+  ...interactive.defaultProps(),
+  ...typographic.defaultProps(),
+  typographic: true,
+}
+
+export default Typography
+
+export const T = props => <Typography {...props} />
+export const Label = props => <Typography {...props} font="label" />
+export const P = props => <Typography {...props} font="paragraph" />
+export const Pre = props => <Typography {...props} font="pre" />
+export const Heading = props => <Typography {...props} font="h1" />
+export const SubHeading = props => <Typography {...props} font="h2" />
---
+++ src/components/atoms/typography/stories.jsx
@@ -0,0 +1,80 @@
+/* eslint-disable react/jsx-one-expression-per-line,jsx-a11y/anchor-is-valid */
+import React from 'react'
+
+import { Box } from '_story'
+import { colors } from 'config'
+
+import Typography, {
+  typographicFonts,
+  T,
+  Label,
+  P,
+  Pre,
+  Heading,
+  SubHeading,
+} from '.'
+
+const Demo = () => (
+  <>
+    <h1>Typography</h1>
+    <p>
+      The Typography component can be used to render any HTML font tag and any
+      font style from the theme.
+    </p>
+    <div>
+      <Typography>default typography</Typography>
+    </div>
+    <h2>Alias'</h2>
+    <p>The Typography component has several alias components.</p>
+    <div>
+      <T>The T component is a one-to-one alias for Typography.</T>
+    </div>
+    <div>
+      <Label>
+        The Label component is an alias using the <em>label</em> font style.
+      </Label>
+    </div>
+    <div>
+      <P>
+        The P component is an alias using the <em>paragraph</em> font style.
+      </P>
+    </div>
+    <div>
+      <Pre>
+        The Pre component is an alias using the <em>pre</em> font style.
+      </Pre>
+    </div>
+    <div>
+      <Heading>
+        The Heading component is an alias using the <em>h1</em> font style.
+      </Heading>
+    </div>
+    <div>
+      <SubHeading>
+        The SubHeading component is an alias using the <em>h2</em> font style.
+      </SubHeading>
+    </div>
+    <h2>Font Prop</h2>
+    <Box>
+      {typographicFonts.map(font => (
+        <div>
+          <Typography font={font}>{font} typography</Typography>
+        </div>
+      ))}
+    </Box>
+    <h2>Color Prop</h2>
+    <Box>
+      {colors.map(color => (
+        <div>
+          <Typography color={color}>{color} typography</Typography>
+        </div>
+      ))}
+    </Box>
+  </>
+)
+
+Demo.story = {
+  name: 'Typography',
+}
+
+export default Demo
