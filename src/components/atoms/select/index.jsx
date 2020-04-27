e01c06665eb757b40198425fe01727a38c78eab5
Aaron Hill <68696c6c@gmail.com>
2020-04-27 00:52:27 -0600

    Added support for an options prop to Select atoms.

--- src/components/atoms/select/index.jsx
+++ src/components/atoms/select/index.jsx
@@ -6,19 +6,35 @@ import { handleProps } from '../../utils'
 import { control } from '../_base/controllable'
 import * as Styled from './styles'
 
-const Select = ({ multiple, ...others }) => {
+const Select = ({ children, id, multiple, options, ...others }) => {
   const Tag = multiple ? Styled.Multiselect : Styled.Select
-  return <Tag {...handleProps(others, 'control')} multiple={multiple} />
+  return (
+    <Tag {...handleProps(others, 'control')} multiple={multiple}>
+      {children ||
+        options.map(({ key, value }) => (
+          <option value={value} key={`select-option-${id}-${key}`}>
+            {key}
+          </option>
+        ))}
+    </Tag>
+  )
 }
 
 Select.propTypes = {
   ...control.propTypes(),
-  multiselect: PropTypes.bool,
+  multiple: PropTypes.bool,
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      key: PropTypes.string,
+      value: PropTypes.oneOf([PropTypes.string, PropTypes.number]),
+    })
+  ),
 }
 
 Select.defaultProps = {
   ...control.defaultProps(),
-  multiselect: false,
+  multiple: false,
+  options: [],
 }
 
 export default Select
