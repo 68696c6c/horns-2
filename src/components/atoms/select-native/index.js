942ac5ff9a1830b0ee06d67db6e92cd7d08b8a15
Aaron Hill <68696c6c@gmail.com>
2020-05-30 20:17:58 -0600

    Fixed bugs in the SelectNative atom multiple prop and height styling.

--- src/components/atoms/select-native/index.js
+++ src/components/atoms/select-native/index.js
@@ -1,8 +1,20 @@
 import styled from '@emotion/styled'
+import { css } from '@emotion/core'
 
 import { select } from '../_base'
 
-const SelectNative = styled.select(...select.styles())
+const SelectNative = styled.select(
+  ...select.styles(),
+  ({ theme, multiple, font }) => {
+    if (multiple) {
+      const f = theme.typography.getStyle(font || 'text')
+      return css`
+        min-height: ${f.lineHeight};
+      `
+    }
+    return null
+  }
+)
 
 SelectNative.propTypes = {
   ...select.propTypes(),
--- src/traits/inline/index.js
+++ src/traits/inline/index.js
@@ -3,16 +3,22 @@ import PropTypes from 'prop-types'
 
 // eslint-disable-next-line import/prefer-default-export
 export const inline = {
-  styles: ({ theme, multiline, font }) => {
+  styles: ({ theme, multiline, multiple, font }) => {
     // Force the element height to match the line-height to ensure that inputs that have controls
     // inside them (e.g. type="datetime-local") don't end up a different size than standard inputs.
     const f = theme.typography.getStyle(font || 'text')
-    return multiline
-      ? null
-      : css`
-          height: ${f.lineHeight};
-          min-width: ${f.lineHeight};
-        `
+    if (multiple) {
+      return css`
+        min-height: ${f.lineHeight};
+      `
+    }
+    return (
+      !multiline &&
+      css`
+        height: ${f.lineHeight};
+        min-width: ${f.lineHeight};
+      `
+    )
   },
   propTypes: () => ({
     multiline: PropTypes.bool,
